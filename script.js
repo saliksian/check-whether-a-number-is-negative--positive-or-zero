6ac5994f93d82b6f5e2a71bfcf5f71135457a47f
Muhammad Salik <75317422+saliksian@users.noreply.github.com>
2021-04-20 12:25:17 +0500

    final commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let number = parseInt(prompt("Enter a number"));
+
+if (number == 0) {
+  console.log("number is zero:" + number);
+} else if (number > 0) {
+  console.log("number is Positive:" + number);
+} else if (number < 0) {
+  console.log("number is negative:" + number);
+} else {
+  console.log("Enter a Valid number!!");
+}
